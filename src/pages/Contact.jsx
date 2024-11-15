dd5b2adbc318bf8bfda59a998b3d1458cb5adb17
mak687 <mak687@hotmail.com>
2024-11-15 17:25:39 +0500

    Pushing files

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,40 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+
+const Contact = () => {
+    return (
+      <div className="container">
+        <h1>Contact Us</h1>
+        <Form>
+
+          <Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+      
+      
+              <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Label>Address</Form.Label>
+                <Form.Control placeholder="1234 Main St" />
+              </Form.Group>
+      
+        
+      
+            </Row>
+    
+          <Form.Group className="mb-3" id="formGridCheckbox">
+            <Form.Check type="checkbox" label="I agree with terms & conditions" />
+          </Form.Group>
+    
+          <Button variant="primary" >
+            Submit
+          </Button>
+        </Form>
+        </div>
+      );
+};
+  
+  export default Contact;
